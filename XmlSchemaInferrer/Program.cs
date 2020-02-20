724a0ef2bdfe0dd409d3746b3326757cf49ba46e
Keith Stacy <keithstacy@users.noreply.github.com>
2020-02-20 10:24:26 -0600

    Add files via upload

---
+++ XmlSchemaInferrer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlSchemaInferrer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new SchemaEngine { FilePath = args[0] }.Run();
+        }
+    }
+}
---
+++ XmlSchemaInferrer/SchemaEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace XmlSchemaInferrer
+{
+    public class SchemaEngine
+    {
+        public string FilePath { get; set; }
+        public void Run()
+        {
+            MemoryStream stream = new MemoryStream();
+            XmlReader reader = XmlReader.Create(FilePath);
+            XmlSchemaSet schemaSet = new XmlSchemaSet();
+            XmlSchemaInference schema = new XmlSchemaInference();
+            schemaSet = schema.InferSchema(reader);
+            foreach (XmlSchema item in schemaSet.Schemas())
+            {
+                item.Write(stream);
+            }
+            File.WriteAllBytes(FilePath + ".xsd", stream.ToArray());
+        }
+    }
+}
